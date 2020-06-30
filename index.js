4dbdb9f4544b6fc4ac87af858522a763d0ed12c4
AKZ <kauffman.andre@gmail.com>
2020-06-29 21:18:02 -0300

    turf and bbox added

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 import L from 'leaflet';
+import turf from 'turf';
 
 var map = L.map("map", {
     crs: L.CRS.Simple,
     minZoom: -3
 });
 
-var myLines = [
-    {
-        type: "LineString",
-        coordinates: [[175.2, 145.0], [41.6, 130.1], [13.4, 56.5]]
-    },
-    {
-        type: "LineString",
-        coordinates: [[0.0, 0.0], [500.0, 500.0]]
-    },
-    {
-        type: "Point",
-        coordinates: [175.2, 145.0],
-    }
-];
-
-var floorplanGeoJson = [
+var floorplan = [
     {type:"Polygon", coordinates: [[[0.0, 0.0],[5.0, 0.0],[5.0, 7.0],[0.0, 7.0],[0.0, 0.0]]]},
     {type:"Polygon", coordinates: [[[5.0, 3.5],[7.5, 3.5],[7.5, 7.0],[5.0, 7.0],[5.0, 3.5]]]},
     {type:"Polygon", coordinates: [[[7.5, 3.5],[10.0, 3.5],[10.0, 7.0],[7.5, 7.0],[7.5, 3.5]]]},
@@ -30,12 +16,16 @@ var floorplanGeoJson = [
     {type:"Polygon", coordinates: [[[7.5, 0.0],[10.0, 0.0],[10.0, 1.25],[7.5, 1.25],[7.5, 0.0]]]},
 ]
 
+var collection = turf.featureCollection(floorplan.map(obj=>turf.feature(obj)));
+
 var myStyle = {
     color: "#f000",
     weight: 25,
     opacity: 0.65
 };
 
-L.geoJSON(floorplanGeoJson).addTo(map);
+L.geoJSON(collection).addTo(map);
 
-map.setView(L.latLng(0.0,0.0), 4);
+var bbox = turf.bbox(collection);
+map.fitBounds([[bbox[0],bbox[1]],[bbox[2],bbox[3]]]);
+
